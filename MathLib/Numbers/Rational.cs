e0cd84ac95622dcffdbd6e1a77ed89605598fcf6
nikitasnt <14nikita14m@gmail.com>
2022-12-19 19:32:50 +0400

    Rational number struct

---
+++ MathLib/Numbers/Rational.cs
@@ -0,0 +1,28 @@
+﻿namespace MathLib.Numbers;
+
+/// <summary>
+/// Represents a rational number.
+/// </summary>
+public struct Rational
+{
+    /// <summary>
+    /// Creates a rational number from a numerator and a denominator.
+    /// </summary>
+    /// <param name="numerator">Numerator.</param>
+    /// <param name="denominator">Denominator.</param>
+    public Rational(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    /// <summary>
+    /// Numerator.
+    /// </summary>
+    public int Numerator { get; }
+
+    /// <summary>
+    /// Denominator.
+    /// </summary>
+    public int Denominator { get; }
+}
