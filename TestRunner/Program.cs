794afbb12b62c34080acbb2f2040659347e39255
nikitasnt <14nikita14m@gmail.com>
2022-12-27 23:01:36 +0400

    Console-project for diff tests

---
+++ TestRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using MathLib.Matrices;
+
+var array = new[,]
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+var matrix = new SquareMatrix<int>(array);
+
+Console.WriteLine(matrix.Determinant());
