26d60352a8475434dfb9290de1d62cd8b2cd9594
nikitasnt <14nikita14m@gmail.com>
2022-12-29 00:50:12 +0400

    Add and sum matrices tests

---
+++ MathLib.Tests/MatrixOfIntTests.cs
@@ -0,0 +1,66 @@
+using MathLib.Matrices;
+
+namespace MathLib.Tests;
+
+public class MatrixTests
+{
+    private Matrix<int> _sourceMatrix = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        var arrForMatrix = new[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        _sourceMatrix = new Matrix<int>(arrForMatrix);
+    }
+
+    [Test]
+    public void AddMatrices()
+    {
+        var arrForMatrix = new[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        var matrix = new Matrix<int>(arrForMatrix);
+
+        var result = _sourceMatrix + matrix;
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.MatrixArray[0, 0], Is.EqualTo(2));
+            Assert.That(result.MatrixArray[0, 1], Is.EqualTo(4));
+            Assert.That(result.MatrixArray[0, 2], Is.EqualTo(6));
+            Assert.That(result.MatrixArray[1, 0], Is.EqualTo(8));
+            Assert.That(result.MatrixArray[1, 1], Is.EqualTo(10));
+            Assert.That(result.MatrixArray[1, 2], Is.EqualTo(12));
+        });
+    }
+
+    [Test]
+    public void SubMatrices()
+    {
+        var arrForMatrix = new[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        var matrix = new Matrix<int>(arrForMatrix);
+
+        var result = _sourceMatrix - matrix;
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.MatrixArray[0, 0], Is.EqualTo(0));
+            Assert.That(result.MatrixArray[0, 1], Is.EqualTo(0));
+            Assert.That(result.MatrixArray[0, 2], Is.EqualTo(0));
+            Assert.That(result.MatrixArray[1, 0], Is.EqualTo(0));
+            Assert.That(result.MatrixArray[1, 1], Is.EqualTo(0));
+            Assert.That(result.MatrixArray[1, 2], Is.EqualTo(0));
+        });
+    }
+}
