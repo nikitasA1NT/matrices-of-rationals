96dd8a6ce1ee703a83aeb10fa8ea4f842ea7f7c2
nikitasnt <14nikita14m@gmail.com>
2022-12-29 00:58:11 +0400

    Multiply matrix tests

--- MathLib.Tests/MatrixOfIntTests.cs
+++ MathLib.Tests/MatrixOfIntTests.cs
@@ -63,4 +63,41 @@ public class MatrixTests
             Assert.That(result.MatrixArray[1, 2], Is.EqualTo(0));
         });
     }
+
+    [Test]
+    public void MultiplyMatrices()
+    {
+        var arrForMatrix = new[,]
+        {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+
+        var matrix = new Matrix<int>(arrForMatrix);
+
+        var result = _sourceMatrix * matrix;
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.MatrixArray[0, 0], Is.EqualTo(22));
+            Assert.That(result.MatrixArray[0, 1], Is.EqualTo(28));
+            Assert.That(result.MatrixArray[1, 0], Is.EqualTo(49));
+            Assert.That(result.MatrixArray[1, 1], Is.EqualTo(64));
+        });
+    }
+
+    [Test]
+    public void MultiplyMatrixByNumber()
+    {
+        var result = _sourceMatrix * 2;
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.MatrixArray[0, 0], Is.EqualTo(2));
+            Assert.That(result.MatrixArray[0, 1], Is.EqualTo(4));
+            Assert.That(result.MatrixArray[0, 2], Is.EqualTo(6));
+            Assert.That(result.MatrixArray[1, 0], Is.EqualTo(8));
+            Assert.That(result.MatrixArray[1, 1], Is.EqualTo(10));
+            Assert.That(result.MatrixArray[1, 2], Is.EqualTo(12));
+        });
+    }
 }
