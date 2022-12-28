a21feaab1750e2ddc1cfecd97eecfb5bd1dd4b59
nikitasnt <14nikita14m@gmail.com>
2022-12-29 00:25:47 +0400

    Unit tests project

---
+++ MathLib.Tests/MatrixTests.cs
@@ -0,0 +1,15 @@
+namespace MathLib.Tests;
+
+public class MatrixTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
