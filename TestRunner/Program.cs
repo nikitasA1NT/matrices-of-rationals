87dd984f285763ff2a1fdbd986bd27ae1bcf5f25
nikitasnt <14nikita14m@gmail.com>
2023-01-05 13:49:56 +0400

    Cleanify TestRunner

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,16 +1,4 @@
 ﻿using MathLib.Matrices;
+using MathLib.Numbers;
 
-var array = new[,]
-{
-    { 1.1, 2, 3 },
-    { 4, 5, 6 },
-    { 7, 8, 9 }
-};
-
-var matrix = new SquareMatrix<double>(array);
-
-Console.WriteLine(matrix.Determinant());
-
-var invertible = matrix.Invertible();
-
-Console.WriteLine(1.9 % 2);
+Console.WriteLine("Hello, World!");
